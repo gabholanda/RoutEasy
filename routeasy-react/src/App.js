342b308032e678c170dd05bacefc2503101f6cf7
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-09-12 02:55:19 -0300

    Added .env & helper functions

--- routeasy-react/src/App.js
+++ routeasy-react/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      peso: '',
+      logradouro: '',
+      numero: '',
+      bairro: '',
+      complemento: '',
+      cidade: '',
+      estado: '',
+      pais: '',
+      geolocalizacao: {
+        latitude: '',
+        longitude: ''
+
+      }
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    axios.get(`${process.env.REACT_APP_API_URL}/create-delivery`)
+      .then()
+      .catch()  
+  }
+
+  getLocation(place) {
+    place.preventDefault();
+    const placeArr = place.target.value.split(' ')
+
+    axios.get(`${process.env.REACT_APP_MAP_URL}
+    address=${placeArr.join('+')}
+    &key=${process.env.REACT_APP_MAP_KEY}`)
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+  }
+  render() {
+    return (
+      <div className="App">
+        <input type="text" onClick={(e) => this.getLocation(e)} />
+      </div>)
+
+  }
 }
+
 
 export default App;
